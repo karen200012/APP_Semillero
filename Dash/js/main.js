b72f69e2dd4bbd6511feb1ecb292c64ae69fd623
karen200012 <karencitaji73@gmail.com>
2020-10-20 10:51:36 -0500

    DashboarDiseño

---
+++ Dash/js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$('.btn-sideBar-SubMenu').on('click', function(){
+		var SubMenu=$(this).next('ul');
+		var iconBtn=$(this).children('.zmdi-caret-down');
+		if(SubMenu.hasClass('show-sideBar-SubMenu')){
+			iconBtn.removeClass('zmdi-hc-rotate-180');
+			SubMenu.removeClass('show-sideBar-SubMenu');
+		}else{
+			iconBtn.addClass('zmdi-hc-rotate-180');
+			SubMenu.addClass('show-sideBar-SubMenu');
+		}
+	});
+	$('.btn-exit-system').on('click', function(){
+		swal({
+		  	title: 'Cerrar Sesion?',
+		  	type: 'warning',
+		  	showCancelButton: true,
+		  	confirmButtonColor: '#03A9F4',
+		  	cancelButtonColor: '#F44336',
+		  	confirmButtonText: '<i class="zmdi zmdi-run"></i> Si,Salir!',
+		  	cancelButtonText: '<i class="zmdi zmdi-close-circle"></i> No, Cancelar!'
+		}).then(function () {
+			window.location.href="../index.html";
+		});
+	});
+	$('.btn-menu-dashboard').on('click', function(){
+		var body=$('.dashboard-contentPage');
+		var sidebar=$('.dashboard-sideBar');
+		if(sidebar.css('pointer-events')=='none'){
+			body.removeClass('no-paddin-left');
+			sidebar.removeClass('hide-sidebar').addClass('show-sidebar');
+		}else{
+			body.addClass('no-paddin-left');
+			sidebar.addClass('hide-sidebar').removeClass('show-sidebar');
+		}
+	});
+	
+	
+});
+
